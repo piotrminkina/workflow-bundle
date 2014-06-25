2fca1a3427b1139a40e745281d8fef10c58568f2
Piotr Minkina <p.minkina@piotrminkina.pl>
2014-06-25 15:59:30 +0200

    Initialized Bundle base file

---
+++ PMDWorkflowBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PMD\WorkflowBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class PMDWorkflowBundle
+ * @package PMD\WorkflowBundle
+ */
+class PMDWorkflowBundle extends Bundle
+{
+}
