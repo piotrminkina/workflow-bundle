f865bc3fd5760fe99d381119726385bf6ab43b7c
Piotr Minkina <p.minkina@piotrminkina.pl>
2014-06-26 10:34:25 +0200

    Added doc comments with authoring and license information

--- PMDWorkflowBundle.php
+++ PMDWorkflowBundle.php
@@ -1,11 +1,22 @@
 <?php
 
+/*
+ * This file is part of the PMDWorkflowBundle package.
+ *
+ * (c) Piotr Minkina <projekty@piotrminkina.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PMD\WorkflowBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Class PMDWorkflowBundle
+ *
+ * @author Piotr Minkina <projekty@piotrminkina.pl>
  * @package PMD\WorkflowBundle
  */
 class PMDWorkflowBundle extends Bundle
--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -1,9 +1,20 @@
 <?php
 
+/*
+ * This file is part of the PMDWorkflowBundle package.
+ *
+ * (c) Piotr Minkina <projekty@piotrminkina.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PMD\WorkflowBundle\Tests;
 
 /**
  * Class TestCase
+ *
+ * @author Piotr Minkina <projekty@piotrminkina.pl>
  * @package PMD\WorkflowBundle\Tests
  */
 class TestCase extends \PHPUnit_Framework_TestCase
