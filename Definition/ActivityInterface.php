749005a80fc7e8b8ca205294b8c04b72e691a759
Piotr Minkina <p.minkina@piotrminkina.pl>
2014-07-10 22:04:59 +0200

    Added states with descriptions to Activity and Definition interfaces

--- Definition/ActivityInterface.php
+++ Definition/ActivityInterface.php
@@ -22,6 +22,29 @@ use Doctrine\Common\Collections\Collection;
 interface ActivityInterface
 {
     /**
+     * The activity instance has been created, but may not yet been activated;
+     * no work item exists for that activity.
+     */
+    const STATE_INACTIVE = 'inactive';
+
+    /**
+     * One or more work items have been created and assigned for processing.
+     */
+    const STATE_ACTIVE = 'active';
+
+    /**
+     * The activity instance is quiescent; no further work items are started
+     * until it is resumed (note that some activities may not be suspendable).
+     */
+    const STATE_SUSPENDED = 'suspended';
+
+    /**
+     * The process instance has achieved its completion conditions and any
+     * post-completion system activities such as audit logging are in progress.
+     */
+    const STATE_COMPLETED = 'completed';
+
+    /**
      * @return string
      */
     public function getName();
--- Definition/DefinitionInterface.php
+++ Definition/DefinitionInterface.php
@@ -14,13 +14,56 @@ namespace PMD\WorkflowBundle\Definition;
 use Doctrine\Common\Collections\Collection;
 
 /**
- * Interface DefinitionInterface
+ * Interface DefinitionInterfaceq
  * 
  * @author Piotr Minkina <projekty@piotrminkina.pl>
  * @package PMD\WorkflowBundle\Definition
  */
 interface DefinitionInterface
 {
+    /**
+     * The process instance has been created, but may not yet be running.
+     */
+    const STATE_INITIATED = 'initiated';
+
+    /**
+     * The process instance has started execution and one or more of its
+     * activities may be started.
+     */
+    const STATE_RUNNING = 'running';
+
+    /**
+     * One or more activities are started and activity instances exist
+     * (further sub-states may be supported by particular implementations to
+     * record more detailed information about active activities).
+     */
+    const STATE_ACTIVE = 'active';
+
+    /**
+     * The process instance is quiescent; no further activities are started
+     * until it is resumed.
+     */
+    const STATE_SUSPENDED = 'suspended';
+
+    /**
+     * The process instance has achieved its completion conditions and any
+     * post-completion system activities such as audit logging are in progress.
+     */
+    const STATE_COMPLETE = 'complete';
+
+    /**
+     * The execution of the process has been stopped (abnormally) due to error
+     * or user request.
+     */
+    const STATE_TERMINATED = 'terminated';
+
+    /**
+     * The process instance has been placed in an indefinite archive state
+     * (but may be retrieved for process resumption - typically supported only
+     * for long-lived processes).
+     */
+    const STATE_ARCHIVED = 'archived';
+
     /**
      * @return string
      */
